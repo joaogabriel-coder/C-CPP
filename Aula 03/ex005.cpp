7f1f9310c9a08ea298e140901d9ecaf311d871ca
João Gabriel <cris27negrao@gmail.com>
2025-03-07 20:53:05 -0300

    aa

--- Aula 03/ex005.cpp
+++ Aula 03/ex005.cpp
@@ -1,23 +1,31 @@
 #include <iostream>
-#include <conio.h>
 #include <stdio.h>
 #include <cstring>
+
 using namespace std;
 
-
-
-int main(){
+int main() {
     char text1[50];
     char text2[50];
     char text3[50];
     int n;
-        cout<<"digite algo até 50 carecteres: ";
-        cin.getline(text2, 50);
-    cout<<"string copiada: "<<strcpy(text1,text2);
 
-    cout<<"Digite um numero menor que 50: ";
-    cin>>n;
-    strcpy(text3, text1, n);
-    text3[n]='\0';
-    cout<<"\n copia 2: "<<text3;
+    cout << "Digite algo ate 50 caracteres: ";
+    cin.getline(text2, 50);  // Permite entrada com espaços
+
+    cout << "String copiada: " << strcpy(text1, text2) << endl;
+
+    cout << "Digite um numero menor que 50: ";
+    cin >> n;
+
+    if (n < 50) {
+        strncpy(text3, text1, n);
+        text3[n] = '\0';  // Garante que a string tenha um terminador nulo
+
+        cout << "\nCopia 2: " << text3 << endl;
+    } else {
+        cout << "Erro: O numero deve ser menor que 50!" << endl;
+    }
+
+    return 0;
 }
